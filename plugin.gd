236a24402e9304e6c3e61855d34b2a57e535ec62
Gede Hari <harikresnagede@gmail.com>
2023-02-25 10:04:02 +0800

    removed ResourceQueue as a singleton
    ill think of something else

--- plugin.gd
+++ plugin.gd
@@ -1,25 +1,23 @@
 tool
 extends EditorPlugin
 
-var dock: Control
-
 
 func _enter_tree() -> void:
 	add_autoload_singleton("Util", "res://addons/SquirrelFramework/scripts/core/Util.gd")
 	add_autoload_singleton("Global", "res://addons/SquirrelFramework/scripts/core/Global.gd")
 	add_autoload_singleton("ViewportManager", "res://addons/SquirrelFramework/scripts/core/ViewportManager.gd")
-	add_autoload_singleton("ResourceQueue", "res://addons/SquirrelFramework/scripts/core/ResourceQueue.gd")
 	add_autoload_singleton("StateManager", "res://addons/SquirrelFramework/scripts/core/StateManager.gd")
 	add_autoload_singleton("Debug", "res://addons/SquirrelFramework/scripts/core/Debug.gd")
 
 	if not ProjectSettings.has_setting("application/config/version"):
 		ProjectSettings.set_setting("application/config/version", "1.0")
-		ProjectSettings.save()
+
+	ProjectSettings.save()
 
 
 func _exit_tree() -> void:
-	remove_autoload_singleton("Global")
-	remove_autoload_singleton("ResourceQueue")
-	remove_autoload_singleton("StateManager")
 	remove_autoload_singleton("Util")
+	remove_autoload_singleton("Global")
+	remove_autoload_singleton("ViewportManager")
+	remove_autoload_singleton("StateManager")
 	remove_autoload_singleton("Debug")
